74356696e04eb4918776c701a4e122e9b4cb225a
Al Prince Llavan <alprincellavan2019@gmail.com>
2024-11-27 00:33:44 +0800

    admin movies crud page

--- im2_project/cinema/cinema_app/views.py
+++ im2_project/cinema/cinema_app/views.py
@@ -1,18 +1,8 @@
 from urllib import request
 from django.shortcuts import get_object_or_404, redirect, render
-from django.http import HttpResponse
-from .models import Genre
-from .forms import GenreForm
-from django.shortcuts import render
-from .models import Movie
-from .forms import MovieForm
-from .models import Branch, Cinema, Cinema_Movie
-from .forms import BranchForm, CinemaForm, CinemaMovieForm
-from .models import Customer
-from .forms import CustomerForm
-from .models import Booking
-from .forms import BookingForm
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Genre, Movie, Branch, Cinema, Cinema_Movie, Customer, Booking
+from .forms import GenreForm, MovieForm, BranchForm, CinemaForm, CinemaMovieForm, CustomerForm, BookingForm
 
 # Create your views here.
 
@@ -25,21 +15,22 @@ def home_page(request):
 def landing_page(request):
     return render(request, 'landing.html')
 
+def admin_dashboard(request):
+    return render(request, 'admin/admin.html')
+
 def genre_list(request):
     genres = Genre.objects.all()
     return render(request, 'genres/genre_list.html', {'genres': genres})
-
 
 def genre_create(request):
     if request.method == 'POST':
         form = GenreForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('genre_list')  
+            return redirect('genre_list')
     else:
         form = GenreForm()
     return render(request, 'genres/genre_form.html', {'form': form})
-
 
 def genre_edit(request, pk):
     genre = get_object_or_404(Genre, pk=pk)
@@ -52,7 +43,6 @@ def genre_edit(request, pk):
         form = GenreForm(instance=genre)
     return render(request, 'genres/genre_form.html', {'form': form})
 
-
 def genre_delete(request, pk):
     genre = get_object_or_404(Genre, pk=pk)
     if request.method == 'POST':
@@ -60,17 +50,20 @@ def genre_delete(request, pk):
         return redirect('genre_list')
     return render(request, 'genres/genre_confirm_delete.html', {'genre': genre})
 
-# def home(request):
-#     return redirect('genre_list')
-
-# MOVIE CRUD
+# Movie CRUD
 def movie_list(request):
     movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+    return render(request, 'admin/movies_admin.html', {'movies': movies})
 
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
-    return render(request, 'movies/movie_detail.html', {'movie': movie})
+    data = {
+        'title': movie.title,
+        'duration': movie.duration,
+        'description': movie.description,
+        'genre': movie.genre.name,
+    }
+    return JsonResponse(data)
 
 def movie_create(request):
     if request.method == 'POST':
@@ -80,7 +73,8 @@ def movie_create(request):
             return redirect('movie_list')
     else:
         form = MovieForm()
-    return render(request, 'movies/movie_form.html', {'form': form})
+    genres = Genre.objects.all()
+    return render(request, 'admin/add_movies_admin.html', {'form': form, 'genres': genres})
 
 def movie_update(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
@@ -88,17 +82,18 @@ def movie_update(request, pk):
         form = MovieForm(request.POST, instance=movie)
         if form.is_valid():
             form.save()
-            return redirect('movie_detail', pk=pk)
+            return redirect('movie_list')
     else:
         form = MovieForm(instance=movie)
-    return render(request, 'movies/movie_form.html', {'form': form})
+    genres = Genre.objects.all()
+    return render(request, 'admin/update_movie_admin.html', {'form': form, 'genres': genres})
 
 def movie_delete(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     if request.method == 'POST':
         movie.delete()
         return redirect('movie_list')
-    return render(request, 'movies/movie_confirm_delete.html', {'movie': movie})
+    return render(request, 'admin/movies_admin.html', {'movie': movie})
 
 def branch_list(request):
     branches = Branch.objects.all()
@@ -161,37 +156,33 @@ def cinema_movie_create(request):
         form = CinemaMovieForm()
     return render(request, 'cinemas/cinema_movie_form.html', {'form': form})
 
-#sign up view
+# Sign up view
 def signup_view(request):
     if request.method == 'POST':
         form = CustomerForm(request.POST)
         if form.is_valid():
             customer = form.save(commit=False)
-            customer.password = form.cleaned_data['password']  
+            customer.password = form.cleaned_data['password']
             customer.save()
-            return redirect('loginform')  
+            return redirect('loginform')
     else:
         form = CustomerForm()
     return render(request, 'user-side/signupform.html', {'form': form})
-
-
 
 def customer_success(request):
     customers = Customer.objects.all()
     return render(request, 'cinema_app/customer_success.html', {'customers': customers})
 
 def update_customer(request, customer_id):
-    customer = get_object_or_404(Customer, customer_id=customer_id) 
+    customer = get_object_or_404(Customer, customer_id=customer_id)
     if request.method == "POST":
         form = CustomerForm(request.POST, instance=customer)
         if form.is_valid():
             form.save()
-            return redirect('customer_success')  
+            return redirect('customer_success')
     else:
         form = CustomerForm(instance=customer)
-    
     return render(request, 'cinema_app/update_customer.html', {'form': form})
-
 
 def delete_customer(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
@@ -200,7 +191,7 @@ def delete_customer(request, customer_id):
         return redirect('customer_success')
     return render(request, 'cinema_app/delete_customer.html', {'customer': customer})
 
-#Booking_views
+# Booking views
 def booking_list(request):
     bookings = Booking.objects.all()
     return render(request, 'cinema_app/booking_list.html', {'bookings': bookings})
@@ -236,12 +227,6 @@ def delete_booking(request, booking_id):
         return redirect('booking_list')
     return render(request, 'booking/booking_delete.html', {'booking': booking})
 
-#login view
+# Login view
 def login_view(request):
     return render(request, 'user-side/login.html')
-
-
-
-  
-
-  

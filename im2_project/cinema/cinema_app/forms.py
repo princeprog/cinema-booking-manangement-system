8961fd7b23506f8cb7c883c91af1bd70c025e879
Al Prince Llavan <alprincellavan2019@gmail.com>
2024-11-27 11:23:50 +0800

    cinema admin basic crud

--- im2_project/cinema/cinema_app/forms.py
+++ im2_project/cinema/cinema_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Genre, Movie, Branch, Cinema, Cinema_Movie, Customer,Booking
+from .models import Genre, Movie, Branch, Cinema, Cinema_Movie, Customer, Booking
 
 class GenreForm(forms.ModelForm):
     class Meta:
@@ -13,36 +13,61 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ['title', 'duration', 'description', 'genre']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter movie title'}),
+            'duration': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter duration in minutes'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter movie description'}),
+            'genre': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 class BranchForm(forms.ModelForm):
     class Meta:
         model = Branch
         fields = ['branch_name']
+        widgets = {
+            'branch_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter branch name'}),
+        }
 
 class CinemaForm(forms.ModelForm):
     class Meta:
         model = Cinema
         fields = ['cinema_name', 'branch']
+        widgets = {
+            'cinema_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter cinema name'}),
+            'branch': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 class CinemaMovieForm(forms.ModelForm):
     class Meta:
         model = Cinema_Movie
-        fields = ['movie_ID', 'cinema_ID', 'showtimes', 'branch']
+        fields = ['movie_ID', 'cinema_ID', 'showtimes']
+        widgets = {
+            'movie_ID': forms.Select(attrs={'class': 'form-control'}),
+            'cinema_ID': forms.Select(attrs={'class': 'form-control'}),
+            'showtimes': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter showtimes'}),
+        }
 
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['firstname', 'lastname', 'username', 'password', 'age', 'address']  # Include all fields
+        fields = ['firstname', 'lastname', 'username', 'password', 'age', 'address']
         widgets = {
-            'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
-            'age': forms.NumberInput(attrs={'placeholder': 'Age', 'min': 0}),
-            'address': forms.TextInput(attrs={'placeholder': 'Address'}),
+            'firstname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter first name'}),
+            'lastname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter last name'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter age'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter address'}),
         }
-
-
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['cinema_movie_id', 'customer_id', 'seat_no', 'date', 'time']
-
+        widgets = {
+            'cinema_movie_id': forms.Select(attrs={'class': 'form-control'}),
+            'customer_id': forms.Select(attrs={'class': 'form-control'}),
+            'seat_no': forms.Select(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Enter date'}),
+            'time': forms.TimeInput(attrs={'class': 'form-control', 'placeholder': 'Enter time'}),
+        }
--- im2_project/cinema/cinema_app/models.py
+++ im2_project/cinema/cinema_app/models.py
@@ -5,7 +5,7 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class Movie(models.Model):
     movieId = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
@@ -15,23 +15,29 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
-    
 
 class Branch(models.Model):
     branch_ID = models.AutoField(primary_key=True)
     branch_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.branch_name
 
 class Cinema(models.Model):
     cinema_ID = models.AutoField(primary_key=True)
     cinema_name = models.CharField(max_length=100)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.cinema_name
+
 class Cinema_Movie(models.Model):
     movie_ID = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cinema_ID = models.ForeignKey(Cinema, on_delete=models.CASCADE)
-    showtimes = models.CharField(max_length=50,null=True)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True)
+    showtimes = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.movie_ID.title} at {self.cinema_ID.cinema_name}"
 
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
@@ -41,6 +47,9 @@ class Customer(models.Model):
     age = models.PositiveBigIntegerField()
     address = models.CharField(max_length=255)
     password = models.CharField(max_length=128, default='defaultpassword')   
+
+    def __str__(self):
+        return self.username
 
 class Seats(models.Model):  
     seat_no = models.AutoField(primary_key=True)
@@ -52,9 +61,3 @@ class Booking(models.Model):
     seat_no = models.ForeignKey(Seats, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
-
-
-
-
-
-
